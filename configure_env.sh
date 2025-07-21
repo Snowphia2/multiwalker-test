84e622dc29856c769838cdd7eb58cfaa56454718
Yu Zhuohao <yuzhuohao2023@iscas.ac.cn>
2025-07-21 10:39:35 +0800

    MAPDN env

--- configure_env.sh
+++ configure_env.sh
@@ -4,3 +4,12 @@ sudo apt-get update
 sudo apt-get install sumo sumo-tools sumo-doc
 echo 'export SUMO_HOME="/usr/share/sumo"' >> ~/.bashrc
 source ~/.bashrc
+
+# Install MAPDN
+cd packages/MAPDN/mapdn/environments/var_voltage_control/data
+gdown https://drive.google.com/file/d/1-GGPBSolVjX1HseJVblNY3KoTqfblmLh/
+view?usp=sharing --fuzzy
+unzip voltage_control_data.zip
+rm voltage_control_data.zip
+mv voltage_control_data/* .
+apt install freeglut3-dev python3-opengl -y
