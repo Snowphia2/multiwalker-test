39400dc0e5dfe4f18582e827aa42bd00b1d1320c
Yu Zhuohao <yuzhuohao2023@iscas.ac.cn>
2025-07-26 09:40:53 +0800

    fix: mapdn

--- packages/HARL/harl/envs/mapdn/mapdn_types.py
+++ packages/HARL/harl/envs/mapdn/mapdn_types.py
@@ -4,7 +4,9 @@ def mapdn_customize_dict(cfg, algo_dict: dict, env_dict: dict, save_group: str):
     if algo_dict["train"].get("episode_length") is not None:
         max_cycles = env_dict["max_cycles"]
         algo_dict["train"]["episode_length"] = max_cycles - 1
-
+    print(
+        f"algo_dict['train']['episode_length'] = {algo_dict['train']['episode_length']}"
+    )
     scenario = env_dict["scenario"]
     if scenario == "case33_3min_final":
         env_dict["action_bias"] = 0.0
--- sources/skill/code/sweep.py
+++ sources/skill/code/sweep.py
@@ -26,12 +26,12 @@ def main():
     parser.add_argument(
         "--agent_id",
         type=str,
-        default="yuzh2001-iscas/harl_mapdn_ppo_sweep/zfsjw4hs",
+        default="yuzh2001-iscas/harl_mapdn_ppo_sweep/5gyktw46",
         help="wandb agent ID",
     )
 
     args = parser.parse_args()
-    n_jobs = 5
+    n_jobs = 24
 
     print(f"启动 {n_jobs} 个并发agent执行: {args.agent_id}")
 
